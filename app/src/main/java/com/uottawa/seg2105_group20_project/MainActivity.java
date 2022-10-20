e4b14635d8b9156c19e4e6ebdac89ebedf6ff590
Adham Radwan <aradw009@uottawa.ca>
2022-10-20 14:31:35 -0400

    #1 Added Firebase Database to Project #2 Added check if both paramters are empty in login #3 Made login authentication check NOT FULLY IMPLEMENTED SINCE REGISTRATION IS NOT STORED IN DATABASE YETgit add .git add .

--- app/src/main/java/com/uottawa/seg2105_group20_project/MainActivity.java
+++ app/src/main/java/com/uottawa/seg2105_group20_project/MainActivity.java
@@ -1,14 +1,26 @@
 package com.uottawa.seg2105_group20_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 //Class for main login page
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://seg2105-group20-default-rtdb.firebaseio.com");
+
     private Button adminButton;
     private Button signupButton;
     private Button loginButton;
@@ -18,11 +30,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        adminButton = (Button) findViewById(R.id.adminButton);
-        signupButton = (Button) findViewById(R.id.signupButton);
-        loginButton = (Button) findViewById(R.id.loginButton);
+        final Button adminButton = findViewById(R.id.adminButton);
+        final Button signupButton = findViewById(R.id.signupButton);
+        final Button loginButton = findViewById(R.id.loginButton);
 
-
+        final EditText loginEmailAddress = findViewById(R.id.loginEmailAddress);
+        final EditText loginPassword = findViewById(R.id.loginPassword);
 
         adminButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,9 +54,50 @@ public class MainActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                loginClick(view);
+
+                final String emailAddressEntered = loginEmailAddress.getText().toString();
+
+                final String passwordEntered = loginPassword.getText().toString();
+
+                if(emailAddressEntered.isEmpty() || passwordEntered.isEmpty()){
+                    Toast.makeText(MainActivity.this,"Please enter email and password", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                            //Check if email address exists in firebase database
+                            if(snapshot.hasChild(emailAddressEntered)){
+                                //email exists in firebase database
+                                //now get the password and match it
+
+                                final String getPasswowrd = snapshot.child(emailAddressEntered).child("password").getValue(String.class);
+
+                                //Check if password also matches email in database
+                                if(getPasswowrd.equals(passwordEntered)){
+                                    loginClick(view);
+                                } else {
+                                    Toast.makeText(MainActivity.this,"Wrong Password entered", Toast.LENGTH_SHORT).show();
+                                }
+                            }else{
+                                Toast.makeText(MainActivity.this,"Wrong Email Address entered", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+                }
+
+
             }
         });
+
+
     }
 
     public void adminClick (View v){
--- app/src/main/java/com/uottawa/seg2105_group20_project/SignUpClient.java
+++ app/src/main/java/com/uottawa/seg2105_group20_project/SignUpClient.java
@@ -19,5 +19,6 @@ public class SignUpClient extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
