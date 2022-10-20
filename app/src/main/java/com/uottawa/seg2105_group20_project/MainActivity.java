dc3174a2ea59c4685639dd9bfb646368ff87083d
Adham Radwan <aradw009@uottawa.ca>
2022-10-20 13:09:08 -0400

    Application now launches through mainActivity and popupmenudeleted

--- app/src/main/java/com/uottawa/seg2105_group20_project/MainActivity.java
+++ app/src/main/java/com/uottawa/seg2105_group20_project/MainActivity.java
@@ -2,7 +2,9 @@ package com.uottawa.seg2105_group20_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 //Class for main login page
 public class MainActivity extends AppCompatActivity {
@@ -12,4 +14,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onClick(View view){
+        switch (view.getId()) {
+            case R.id.adminButton:
+                adminClick(view);
+                break;
+            case R.id.signupButton:
+                signUpClick(view);
+                break;
+            case R.id.loginButton:
+                loginClick(view);
+                break;
+        }
+    }
+
+    public void adminClick (View v){
+        Intent i = new Intent(this, LoginAdmin.class);
+        startActivity(i);
+    }
+
+    public void signUpClick (View v) {
+        Intent i = new Intent(this, SignupRole.class);
+        startActivity(i);
+    }
+
+    public void loginClick (View v){
+        Intent i = new Intent(this, WelcomePage.class);
+        startActivity(i);
+    }
+
 }
--- app/src/main/java/com/uottawa/seg2105_group20_project/SignUpClient.java
+++ app/src/main/java/com/uottawa/seg2105_group20_project/SignUpClient.java
@@ -18,66 +18,6 @@ public class SignUpClient extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(layout.activity_signup_client);
-
-        EditText firstName = (EditText) findViewById(id.firstname);
-
-        EditText lastName = (EditText) findViewById(id.lastname);
-
-        EditText emailAddress = (EditText) findViewById(id.emailaddress);
-
-        EditText password = (EditText) findViewById(id.password);
-
-        EditText retypePassword = (EditText) findViewById(id.repassword);
-
-        Button signupButton = (Button) findViewById(id.signupbtn);
-
-        signupButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String pwd = password.getText().toString();
-                String repwd = retypePassword.getText().toString();
-
-
-                if (pwd != repwd) {
-                    Toast.makeText(SignUpClient.this, "Password do not match", Toast.LENGTH_SHORT);
-                    password.setText(null);
-                    retypePassword.setText(null);
-                } else {
-                    //Save User info to database
-
-                    //Display successful signup message
-                    Toast.makeText(SignUpClient.this, "Signup Successful!", Toast.LENGTH_SHORT);
-
-                    //Ask if he wants to sign up as a client or cook
-
-                    PopupMenu popupMenu = new PopupMenu(SignUpClient.this,signupButton);
-
-                    popupMenu.getMenuInflater().inflate(menu.popup_menu, popupMenu.getMenu());
-
-                    popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                        
-                        public boolean onMenuItemClick(MenuItem item) {
-
-                            Toast.makeText(SignUpClient.this, "You Clicked : " + item.getTitle(), Toast.LENGTH_SHORT).show();
-                            return true;
-
-                        }
-                    });
-
-                    popupMenu.show();
-
-                    //If item selected is client
-//\\                    if(){
-//                        //redirect to client page
-//                    }else{
-//                        //else redirect to cook page
-//                    }
-                }
-
-            }
-        });
-
-
+        setContentView(R.layout.activity_main);
     }
 }
