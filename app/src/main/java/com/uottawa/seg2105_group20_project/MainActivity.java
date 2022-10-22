c13480c9fa036d8c361caf2d86f7b7552d895a78
Maisam Al-Nuaimi <malnu061@uottawa.ca>
2022-10-21 22:46:35 -0400

    Add new pages

--- app/src/main/java/com/uottawa/seg2105_group20_project/MainActivity.java
+++ app/src/main/java/com/uottawa/seg2105_group20_project/MainActivity.java
@@ -113,14 +113,14 @@ public class MainActivity extends AppCompatActivity {
         boolean userExist = false;
         for(int i = 0; i < clients.size(); i++){
             if(clients.get(i).email.trim().equals(email) && clients.get(i).password.trim().equals(password)){
-                intent = new Intent(this, WelcomePage.class);
+                intent = new Intent(this, WelcomePageClient.class);
                 startActivity(intent);
                 userExist = true;
             }
         }
         for(int i = 0; i < cooks.size(); i++){
             if(cooks.get(i).email.trim().equals(email) && cooks.get(i).password.trim().equals(password)){
-                intent = new Intent(this, WelcomePage.class);
+                intent = new Intent(this, WelcomePageCook.class);
                 startActivity(intent);
                 userExist = true;
             }
