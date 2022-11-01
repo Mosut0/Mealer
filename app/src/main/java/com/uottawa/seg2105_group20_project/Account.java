e53702c94bdaa0fbbf04b45247e71a98104227cf
Mosut0 <90711946+Mosut0@users.noreply.github.com>
2022-11-01 14:25:57 -0400

    Delete app directory

--- app/src/main/java/com/uottawa/seg2105_group20_project/Account.java
+++
@@ -1,9 +0,0 @@
-package com.uottawa.seg2105_group20_project;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-//Class for admin login page
-public class Account extends AppCompatActivity {
-    public String id, firstName, lastName, email, password, address;
-
-}
--- app/src/main/java/com/uottawa/seg2105_group20_project/Admin.java
+++
@@ -1,9 +0,0 @@
-package com.uottawa.seg2105_group20_project;
-
-public class Admin extends Account {
-    public String userName, password;
-    public Admin(String userName, String password){
-        this.userName = userName;
-        this.password = password;
-    }
-}
