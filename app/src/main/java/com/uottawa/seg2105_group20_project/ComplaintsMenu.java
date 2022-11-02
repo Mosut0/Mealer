330b88687a0e8c25ef0972e5e8c93c988ffbcc32
Mostafa <mos.yas123@gmail.com>
2022-11-02 00:22:57 -0400

    added complaint list page

---
+++ app/src/main/java/com/uottawa/seg2105_group20_project/ComplaintsMenu.java
@@ -0,0 +1,14 @@
+package com.uottawa.seg2105_group20_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ComplaintsMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complaints_menu);
+    }
+}
