b8294a0c8e36904ee05b840618b09aa812d2952b
Maisam Al-Nuaimi <malnu061@uottawa.ca>
2022-11-18 18:49:00 -0500

    Added 5 test cases for Deliverable 3

--- app/src/test/java/com/uottawa/seg2105_group20_project/MealerUnitTests.java
+++ app/src/test/java/com/uottawa/seg2105_group20_project/MealerUnitTests.java
@@ -1,6 +1,8 @@
 package com.uottawa.seg2105_group20_project;
 
 import org.junit.Test;
+
+import java.net.PortUnreachableException;
 import java.util.*;
 import static org.junit.Assert.*;
 
@@ -39,6 +41,7 @@ public class MealerUnitTests {
         return databaseClient;
     }
 
+
     /**
      * Generates list of cooks that act as a database.
      * @return databaseCook
@@ -64,6 +67,53 @@ public class MealerUnitTests {
 
         return databaseCook;
     }
+
+    /**
+     * Generates list of meals that act as a database.
+     * @return databaseMeal
+     */
+
+    public static List<Meal> generateMealDB(){
+        List<Meal> databaseMeal = new ArrayList<>();
+
+        Meal mealTest1 = new Meal("test1", "test1", "test1", "test1", "test1", "test1", "test1", "test1", "test1", true);
+        Meal mealTest2 = new Meal("test2", "test2", "test2", "test2", "test2", "test2", "test2", "test2", "test2", true);
+        Meal mealTest3 = new Meal("test3", "test3", "test3", "test3", "test3", "test3", "test3", "test3", "test3", true);
+        Meal mealTest4 = new Meal("test4", "test4", "test4", "test4", "test4", "test4", "test4", "test4", "test4", true);
+        Meal mealTest5 = new Meal("test5", "test5", "test5", "test5", "test5", "test5", "test5", "test5", "test5", true);
+        Meal mealTest6 = new Meal("test6", "test6", "test6", "test6", "test6", "test6", "test6", "test6", "test6", true);
+        Meal mealTest7 = new Meal("test7", "test7", "test7", "test7", "test7", "test7", "test7", "test7", "test7", true);
+
+        databaseMeal.add(mealTest1);
+        databaseMeal.add(mealTest2);
+        databaseMeal.add(mealTest3);
+        databaseMeal.add(mealTest4);
+        databaseMeal.add(mealTest5);
+        databaseMeal.add(mealTest6);
+        databaseMeal.add(mealTest7);
+
+        return databaseMeal;
+    }
+
+    /**
+     * Generates list of complaints that act as a database.
+     * @return databasecomplaint
+     */
+
+    public static List<Complaint> generateComplaintDB(){
+        List<Complaint> databaseComplaint = new ArrayList<>();
+
+        Complaint complaintTest1 = new Complaint("test1", "test1", "test1", "test1");
+        Complaint complaintTest2 = new Complaint("test2", "test2", "test2", "test2");
+        Complaint complaintTest3 = new Complaint("test3", "test3", "test3", "test3");
+        Complaint complaintTest4 = new Complaint("test4", "test4", "test4", "test4");
+        Complaint complaintTest5 = new Complaint("test5", "test5", "test5", "test5");
+        Complaint complaintTest6 = new Complaint("test6", "test6", "test6", "test6");
+        Complaint complaintTest7 = new Complaint("test7", "test7", "test7", "test7");
+
+        return databaseComplaint;
+    }
+
 
     /**
      * Random string generator
@@ -132,6 +182,44 @@ public class MealerUnitTests {
         Cook cook = new Cook(id, fName, lName, email, password, address, description, cheque, new ArrayList<>(), new ArrayList<>());
 
         return cook;
+    }
+
+    /**
+     *Generates a meal
+     * @return meal
+     */
+    public static Meal generateMeal(){
+
+        String id = getAlphaNumericString(10);
+        String mName = getAlphaNumericString(10);
+        String mType = getAlphaNumericString(10);
+        String cuisType = getAlphaNumericString(10);
+        String ingredients = getAlphaNumericString(10);
+        String allergens = getAlphaNumericString(10);
+        String price = getAlphaNumericString(10);
+        String description = getAlphaNumericString(10);
+        String cookID = getAlphaNumericString(10);
+
+        Meal meal = new Meal(id, mName, mType, cuisType, ingredients, allergens, price, description, cookID, true);
+
+        return meal;
+    }
+
+    /**
+     *Generates a complaint
+     * @return complaint
+     */
+
+    public static Complaint generateComplaint(){
+
+        String complainee = getAlphaNumericString(10);
+        String complaineeID = getAlphaNumericString(10);
+        String complainant = getAlphaNumericString(10);
+        String description = getAlphaNumericString(10);
+
+        Complaint complaint = new Complaint(complainee, complaineeID, complainant, description);
+
+        return complaint;
     }
 
     /**
@@ -251,5 +339,87 @@ public class MealerUnitTests {
     /**
      * DELIVERABLE 3 UNIT TESTS
      */
+
+
+    @Test
+    public void checkMealExists(){
+        List<Meal> mealDB = generateMealDB();
+        Meal mealTest4 = new Meal("test4", "test4", "test4", "test4", "test4", "test4", "test4", "test4", "test4", true);
+
+        boolean mealFound = false;
+        for (int i=0; i<mealDB.size(); i++){
+            if (mealDB.get(i).getMealID().trim().equals(mealDB.get(i).getMealID()) && mealDB.get(i).getMealName().trim().equals(mealDB.get(i).getMealName())){
+                mealFound = true;
+                break;
+            };
+        }
+        assertTrue(mealFound);
+
+    }
+
+    @Test
+    public void checkNonExistingMeal() {
+        List<Meal> mealDB = generateMealDB();
+        Meal mealTest4 = new Meal("test4", "test4", "test4", "test77", "test4", "test4", "test4", "test4", "test4", true);
+
+        boolean mealFound = false;
+        for (int i = 0; i < mealDB.size(); i++) {
+            if (mealDB.get(i).getMealID().trim().equals(mealDB.get(i).getMealID()) && mealDB.get(i).getMealName().trim().equals(mealDB.get(i).getMealName())) {
+                mealFound = true;
+                break;
+            }
+            ;
+        }
+        assertFalse(mealFound);
+
+    }
+
+
+    @Test
+    public void checkMealsOffered() {
+        List<Meal> mealDB = generateMealDB();
+
+        int count = 0;
+        for (int i=0; i<mealDB.size(); i++){
+            if(mealDB.get(i).isOffered());
+
+            count++;
+        }
+        assertTrue(count == 7);
+
+    }
+
+
+    @Test
+    public void checkComplaintExists(){
+        List<Complaint> complaintDB = generateComplaintDB();
+        Complaint complaintTest4 = new Complaint("test4", "test4", "test4", "test4");
+
+        boolean complaintFound = false;
+        for (int i=0; i<complaintDB.size(); i++){
+                if (complaintDB.get(i).getComplainee().trim().equals(complaintDB.get(i).getComplainee()) && complaintDB.get(i).getComplaineeID().trim().equals(complaintDB.get(i).getComplaineeID())){
+                complaintFound = true;
+                break;
+            };
+        }
+        assertTrue(complaintFound);
+
+    }
+
+    @Test
+    public void checkNonExistingComplaint(){
+        List<Complaint> complaintDB = generateComplaintDB();
+        Complaint complaintTest4 = new Complaint("ABCD6", "test4", "test4", "test4");
+
+        boolean complaintFound = false;
+        for (int i=0; i<complaintDB.size(); i++){
+            if (complaintDB.get(i).getComplainee().trim().equals(complaintDB.get(i).getComplainee()) && complaintDB.get(i).getComplaineeID().trim().equals(complaintDB.get(i).getComplaineeID())){
+                complaintFound = true;
+                break;
+            };
+        }
+        assertFalse(complaintFound);
+
+    }
 
 }
