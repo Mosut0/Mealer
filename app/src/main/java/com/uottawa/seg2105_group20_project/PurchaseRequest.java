3591969a8469beda554958d8e5c5f548de57ff02
Mohamad Bayaa <mbaya053@uottawa.ca>
2022-12-07 19:57:03 -0500

    Added purchase request class

--- app/src/main/java/com/uottawa/seg2105_group20_project/PurchaseRequest.java
+++ app/src/main/java/com/uottawa/seg2105_group20_project/PurchaseRequest.java
@@ -1,5 +1,24 @@
 package com.uottawa.seg2105_group20_project;
 
 public class PurchaseRequest {
+    public String id;
+    public String cookId;
+    public String clientId;
+    public String mealId;
+    public String pickupTime;
+    public String status;
+    public boolean notificationSent, rated, complained;
 
+    public PurchaseRequest(){}
+    public PurchaseRequest(String id, String cookId, String clientId, String mealId, String pickupTime){
+        this.id= id;
+        this.cookId= cookId;
+        this.clientId= clientId;
+        this.mealId= mealId;
+        this.pickupTime= pickupTime;
+        this.status="Pending";
+        this.notificationSent = false;
+        this.rated = false;
+        this.complained = false;
+    }
 }
